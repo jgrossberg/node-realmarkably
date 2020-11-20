2480931bfd7aba918435454c483f5fa7bc9036ea
Jonah Grossberg <jonahag@gmail.com>
2020-11-19 20:43:50 -0500

    Add autocomplete, add prettier, put form data into js

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-var path =  require('path');
+var path = require("path");
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.send("Fuck off");
+});
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/real', (req, res) => {
-  res.sendFile('./public/real.html', {root: __dirname});
-})
+app.get("/real", (req, res) => {
+  res.sendFile("./public/real.html", { root: __dirname });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- public/javascripts/real.js
+++ public/javascripts/real.js
@@ -1,4 +1,92 @@
+let autocomplete;
+let placeSearch;
+
+const componentForm = {
+  street_number: "short_name",
+  route: "long_name",
+  locality: "long_name",
+  administrative_area_level_1: "short_name",
+  country: "long_name",
+  postal_code: "short_name",
+};
+
+const BEDROOMS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const BATHROOMS = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0];
+const FEATURES = [
+  "Beach",
+  "Downtown",
+  "Mountains",
+  "Lake",
+  "Portland",
+  "Secluded",
+  "Cul de Sac",
+  "Near a park",
+  "Pool",
+  "Basement",
+  "Garage",
+  "Fireplace",
+];
+
 function handleSubmit(evt) {
-    console.log('tryin')
-    document.getElementById('copyOutput').innerText = "place is a dump";
+  document.getElementById("copyOutput").innerText = "place is a dump";
+}
+
+function loadFormData() {
+  const bedroomCountSelect = document.getElementById("inputBedrooms");
+  BEDROOMS.forEach((count) => {
+    let el = document.createElement("OPTION");
+    el.value = count;
+    el.innerText = count;
+    bedroomCountSelect.appendChild(el);
+  });
+
+  const bathroomCountSelect = document.getElementById("inputBathrooms");
+  BATHROOMS.forEach((count) => {
+    let el = document.createElement("OPTION");
+    el.value = count;
+    el.innerText = count;
+    bathroomCountSelect.appendChild(el);
+  });
+
+  const featuresContainer = document.getElementById("features");
+  FEATURES.forEach((feat) => {
+    let parentContainer = document.createElement("DIV");
+    parentContainer.classList = "form-check form-check-inline";
+    let checkboxInput = document.createElement("INPUT");
+    checkboxInput.type = "checkbox";
+    checkboxInput.classList = "form-check-input";
+    checkboxInput.id = feat;
+    let label = document.createElement("LABEL");
+    label.classList = "form-check-label";
+    label.for = feat;
+    label.innerText = feat;
+
+    parentContainer.appendChild(checkboxInput);
+    parentContainer.appendChild(label);
+    featuresContainer.appendChild(parentContainer);
+  });
+}
+
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete"),
+    { types: ["geocode"] }
+  );
+  autocomplete.setFields(["address_component"]);
+}
+
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      const circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy,
+      });
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
 }
